af6b265158050d6c22a2abe3fd16d87362aa2387
unknown <vladancupric@gmail.com>
2022-02-12 20:51:47 +0100

    New commits

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Header />
+    <div className="app">
+    
      
     </div>
+    </>
   );
 }
 
