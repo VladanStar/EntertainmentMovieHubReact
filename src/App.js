902c88523a887c73d3ede6b55116cce9df5258bb
unknown <vladancupric@gmail.com>
2022-02-13 09:18:07 +0100

    New commits

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header/Header";
+import SimpleBottomNavigation from "./components/MainNav/MainNav";
+import { Container } from "@material-ui/core";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    <div className="app">
-    
-     
-    </div>
-    </>
+      <div className="app">
+        <Container>
+          <Routes>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Routes>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
   );
 }
 
